2d400d66eb8b90976c64bbd614e18e23616041fa
GaryJ <410461171@qq.com>
2016-02-26 11:43:33 +0800

    submit the base application and network detect broadcast receiver

---
+++ myandroidlib/src/main/java/com/jijunjie/myandroidlib/base/BaseApplication.java
@@ -0,0 +1,7 @@
+package com.jijunjie.myandroidlib.base;
+
+/**
+ * Created by jijunjie on 16/2/26.
+ */
+public class BaseApplication {
+}
---
+++ myandroidlib/src/main/java/com/jijunjie/myandroidlib/network/InstantNetworkChangeManager.java
@@ -0,0 +1,7 @@
+package com.jijunjie.myandroidlib.network;
+
+/**
+ * Created by jijunjie on 16/2/26.
+ */
+public class InstantNetworkChangeManager {
+}
